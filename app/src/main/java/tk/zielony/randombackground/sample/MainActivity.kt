3a172ce540fb655a8798001d73151de2174e06ee
Zielony <niewartotupisac@gmail.com>
2018-12-14 18:22:45 +0100

    updated sample, added hexagons, added taking colors from the current theme, added screenshots, added glow

--- app/src/main/java/tk/zielony/randombackground/sample/MainActivity.kt
+++ app/src/main/java/tk/zielony/randombackground/sample/MainActivity.kt
@@ -1,9 +1,13 @@
 package tk.zielony.randombackground.sample
 
+import android.animation.ArgbEvaluator
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import carbon.Carbon.getThemeColor
 import kotlinx.android.synthetic.main.activity_main.*
 import tk.zielony.randombackground.RandomBackground
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val b = RandomBackground()
-        background.setBackgroundDrawable(b)
+        generate()
 
         generate.setOnClickListener {
-            b.generate()
+            generate()
             background.postInvalidate()
         }
+    }
+
+    private fun generate(){
+        val random = Random()
+        val colors = arrayOf(Color.RED, Color.GRAY, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.YELLOW)
+
+        val particleColor = arrayOf(getThemeColor(this, android.R.attr.colorForeground), getThemeColor(this, android.R.attr.colorForegroundInverse))[random.nextInt(2)]
+        val color = ArgbEvaluator().evaluate(0.3f, particleColor, colors[random.nextInt(colors.size)]) as Int
+        val intensity = random.nextFloat()/2
+        val style = RandomBackground.Style.values()[random.nextInt(RandomBackground.Style.values().size)]
+        val count = random.nextInt(10) + 5
+
+        val b = RandomBackground(applicationContext, color, particleColor, intensity, style, count)
+        background.setBackgroundDrawable(b)
     }
 }
--- lib/src/main/java/tk/zielony/randombackground/RandomBackground.kt
+++ lib/src/main/java/tk/zielony/randombackground/RandomBackground.kt
@@ -1,67 +1,118 @@
 package tk.zielony.randombackground
 
 import android.animation.ArgbEvaluator
+import android.content.Context
 import android.graphics.*
 import android.graphics.drawable.Drawable
+import android.util.TypedValue
 import java.util.*
+
 
 class RandomBackground : Drawable {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var bitmap: Bitmap? = null
     private var color: Int
     private val random = Random()
+    private val intensity: Float
     private var style: Style
-    private val count: Int
-
-    private val colors = arrayOf(Color.RED, Color.GRAY, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.YELLOW)
+    private var count: Int
+    private var particleColor: Int
+    private val context: Context
 
     enum class Style {
         RECT, CIRCLE, HEX
     }
 
-    constructor() {
-        color = ArgbEvaluator().evaluate(0.2f, Color.WHITE, 0xff008577.toInt()) as Int
+    constructor(context: Context) {
+        this.context = context
+        particleColor = arrayOf(
+            getThemeColor(context, android.R.attr.colorForeground),
+            getThemeColor(context, android.R.attr.colorForegroundInverse)
+        )[random.nextInt(2)]
+        intensity = 0.3f
+        color = ArgbEvaluator().evaluate(intensity, particleColor, getThemeColor(context, R.attr.colorPrimary)) as Int
         style = Style.values()[random.nextInt(Style.values().size)]
         count = random.nextInt(10) + 5
     }
 
-    constructor(color: Int, style: Style, count: Int) {
+    constructor(context: Context, color: Int, particleColor: Int, intensity: Float, style: Style, count: Int) {
+        this.context = context
         this.color = color
+        this.particleColor = particleColor
+        this.intensity = Math.max(0.0f, Math.min(intensity, 1.0f))
         this.style = style
         this.count = count
     }
 
-    fun generate() {
-        color = ArgbEvaluator().evaluate(0.2f, Color.WHITE, colors[random.nextInt(colors.size)]) as Int
+    private fun getThemeColor(context: Context, attr: Int): Int {
+        val theme = context.theme
+        val typedValue = TypedValue()
+        theme.resolveAttribute(attr, typedValue, true)
+        return if (typedValue.resourceId != 0) context.resources.getColor(typedValue.resourceId) else typedValue.data
+    }
 
+    fun generate() {
         val p = Paint()
         val width = bounds.width()
         val height = bounds.height()
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap!!)
         val gradient =
-            LinearGradient(0.0f, 0.0f, 0.0f, height.toFloat(), Color.WHITE, color, Shader.TileMode.CLAMP)
+            LinearGradient(0.0f, 0.0f, 0.0f, height.toFloat(), particleColor, color, Shader.TileMode.CLAMP)
         p.shader = gradient
         canvas.drawRect(0.0f, 0.0f, width.toFloat(), height.toFloat(), p)
 
         p.shader = null
-        p.color = 0xffffffff.toInt()
-        //p.setShadowLayer(20.0f, 0.0f, 5.0f, 0x7f000000)
-        p.alpha = 0x2f
+        p.color = particleColor
+        p.setShadowLayer(
+            context.resources.getDimension(R.dimen.randomBackground_blurSize),
+            0.0f,
+            context.resources.getDimension(R.dimen.randomBackground_blurOffset),
+            particleColor
+        )
+        p.alpha = (0xff * intensity).toInt()
 
         val min = Math.min(width, height) / 6
         for (i in 0 until count) {
-            //  canvas.saveLayer(0.0f,0.0f,width.toFloat(), height.toFloat(),p, Canvas.ALL_SAVE_FLAG)
-            //p.alpha = 0xff
-            canvas.drawCircle(
-                random.nextInt(width).toFloat(),
-                random.nextInt(height).toFloat(),
-                (random.nextInt(min) + min).toFloat(),
-                p
-            )
-            //p.alpha = 0x2f
-            //canvas.restore()
+            when (style) {
+                Style.CIRCLE ->
+                    canvas.drawCircle(
+                        random.nextInt(width).toFloat(),
+                        random.nextInt(height).toFloat(),
+                        (random.nextInt(min) + min).toFloat(),
+                        p
+                    )
+                Style.RECT -> {
+                    val x = random.nextInt(width).toFloat()
+                    val y = random.nextInt(height).toFloat()
+                    val size = (random.nextInt(min) + min).toFloat()
+
+                    canvas.drawRect(x - size / 2, y - size / 2, x + size / 2, y + size / 2, p)
+                }
+                Style.HEX -> {
+                    val x = random.nextInt(width).toFloat()
+                    val y = random.nextInt(height).toFloat()
+                    val size = (random.nextInt(min) + min).toFloat()
+                    canvas.save()
+                    canvas.translate(x, y)
+                    canvas.drawPath(getHexagonPath(size), p)
+                    canvas.restore()
+                }
+            }
         }
+    }
+
+    private fun getHexagonPath(radius: Float): Path {
+        val triangleHeight = (Math.sqrt(3.0) * radius / 2).toFloat()
+        val path = Path()
+        path.moveTo(0.0f, radius)
+        path.lineTo(-triangleHeight, radius / 2)
+        path.lineTo(-triangleHeight, -radius / 2)
+        path.lineTo(0.0f, -radius)
+        path.lineTo(triangleHeight, -radius / 2)
+        path.lineTo(triangleHeight, radius / 2)
+        path.moveTo(0.0f, radius)
+        return path
     }
 
     override fun setBounds(bounds: Rect) {
